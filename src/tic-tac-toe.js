6ab96723fe66b8f163a2f9509af7ac2b001e9758
Abzal <Abzal.Tankibayev@outlook.com>
2019-10-04 16:12:55 +0600

    feat: add task

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,34 +1,37 @@
 class TicTacToe {
-    constructor() {
-
+    constructor(columns =3, rows = 3) {
+        this.field = Array.from({ length: columns }, () => Array.from({ length: rows }, () => null));
+        this.turn = 0;
+        this.MaxTurn = 9;       
     }
-
-    getCurrentPlayerSymbol() {
-
+    getCurrentPlayerSymbol() {       
+        return !(this.turn % 2) && 'x' || 'o';        
     }
-
-    nextTurn(rowIndex, columnIndex) {
-
+    nextTurn(rowIndex, columnIndex) {       
+        (this.field[rowIndex][columnIndex] == null) && (this.field[rowIndex][columnIndex] = this.getCurrentPlayerSymbol()) && (this.turn += 1);
     }
-
-    isFinished() {
-
+    isFinished() {       
+        return (this.noMoreTurns() || this.getWinner()) && true || false;
     }
-
     getWinner() {
-
+        let result = null;       
+        if(this.field[0][0] == this.field[1][1] && this.field[1][1] == this.field[2][2]) {result = this.field[0][0];}
+        else if(this.field[2][0] == this.field[1][1] && this.field[1][1] == this.field[0][2]) {result = this.field[2][0];}
+        else 
+        for(let x=0; x<3; x++) {
+            (this.field[x][0] == this.field[x][1] && this.field[x][1] == this.field[x][2]) && (result = this.field[x][0]) ||            
+            (this.field[0][x] == this.field[1][x] && this.field[1][x] == this.field[2][x]) && (result = this.field[0][x]);                      
+        };
+        return result;
     }
-
-    noMoreTurns() {
-
+    noMoreTurns() {        
+        return (this.turn == this.MaxTurn) && true || false;
     }
-
-    isDraw() {
-
+    isDraw() {               
+        return (this.noMoreTurns() && !this.getWinner()) && true || false;
     }
-
-    getFieldValue(rowIndex, colIndex) {
-
+    getFieldValue(rowIndex, colIndex) {      
+        return this.field[rowIndex][colIndex];
     }
 }
 
